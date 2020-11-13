63cbcf4634e2a5ed3e679ff63a9168c68fcbdb1f
Minjeong <mathmjseo@naver.com>
2020-11-13 21:32:59 +0900

    프로젝트 기본 세팅, 파일 업로드 구현

--- web/page/views.py
+++ web/page/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST' and request.FILES['photo']:
+        upload_file = request.FILES['photo']
+        media_dir = 'media/page/files/'
+        fs = FileSystemStorage(location=media_dir)
+        fs.save(upload_file.name, upload_file)
+    return render(request, 'index.html')
+
+
+def show(request):
+    return render(request, 'show.html')
